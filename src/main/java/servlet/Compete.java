d231d1c8093f401ed30983fd130f1eb8a5798d17
Samuel <css466_f16_seaton@mail.com>
2017-03-16 13:08:30 -0500

    ready to deploy

--- src/main/java/servlet/Compete.java
+++ src/main/java/servlet/Compete.java
@@ -89,7 +89,7 @@ public class Compete extends HttpServlet
       if ( userExists && passwordsMatch )
         {
         session.setAttribute ( userIdAttribute, username ) ;
-        out.print  (  "        <tr style='font-size:x-large;'><td>Hello, " + username + ".</td></tr>\n" 
+        out.print  (  "        <h1 style='font-size:x-large;'>Hello, " + username + ".</h1>\n" 
      /*            +    "        <tr style='font-size:x-large;'><td>The time is " + (new java.util.Date()) + ".</td></tr>\n"
                  +    "        <tr style='font-size:x-large;'><td>This is a new session.</td></tr>\n"
                  +    "        <tr style='font-size:x-large;'><td>The session ID is " + sessionId + ".</td></tr>\n"  ) ; */
