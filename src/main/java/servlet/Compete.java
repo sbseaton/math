2120cf00bc6dae57200119443754843a4bec6f43
Samuel <css466_f16_seaton@mail.com>
2017-03-19 00:41:19 -0500

    ready to deploy

--- src/main/java/servlet/Compete.java
+++ src/main/java/servlet/Compete.java
@@ -122,7 +122,7 @@ public class Compete extends HttpServlet
 				+ 		"<p> " + questionOneRS.getObject("Foil1_Choice_ID") + "</p> " 
 				
 				+		"<form>"
-				+		"<p>hello</p>"
+				+		"<input type=\"radio\" name=\"number\" value=\"150\">" + questionOneRS.getObject("Foil1_Choice_ID") + "<br>  "
 				+		"</form>" );
 				
 				
