ff851831eb8daaa71c1df9671843130c16c4be65
Samuel <css466_f16_seaton@mail.com>
2017-03-28 14:02:24 -0500

    ready

--- src/main/java/servlet/Compete.java
+++ src/main/java/servlet/Compete.java
@@ -172,7 +172,7 @@ public class Compete extends HttpServlet
 				// print each answer with a radio button for user selection, assign the value to the id of the answer
 
         System.out.println( ""
-                +   "    <form method='POST'>\n"
+                +   "    <form method='POST'>\n" ); 
 
 				while (questionOneAnswers.next() )
 				{
