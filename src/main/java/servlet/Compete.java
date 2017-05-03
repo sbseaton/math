61178e644d698ca0b0ffae44cb9ad05861aa3d12
Samuel <css466_f16_seaton@mail.com>
2017-05-02 19:03:06 -0500

    ready

--- src/main/java/servlet/Compete.java
+++ src/main/java/servlet/Compete.java
@@ -392,8 +392,8 @@ public class Compete extends HttpServlet
                     }// end inner if else
                 }// end outer if-else
     
-                String tempQuestionText = (String) easyQuestionsResultSet.getObject("QuestionText") ;   
-                int    tempQuestionID   = Integer.parseInt ( "" + easyQuestionsResultSet.getObject("id") ) ;
+                String questionTextEasy = (String) easyQuestionsResultSet.getObject("QuestionText") ;   
+                int    questionIDEasy   = Integer.parseInt ( "" + easyQuestionsResultSet.getObject("id") ) ;
                 out.println ( " <li class='" + liClasses + "' ><a data-target='/Compete' href='/Compete?Q_ID="+ questionIDEasy +"'> " + questionTextEasy + " </a></li> \n" );
             
  
