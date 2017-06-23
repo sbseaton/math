63f38fdad1819393ae1f65fddf3ddf02efac8c75
Samuel <css466_f16_seaton@mail.com>
2017-06-22 21:13:20 -0500

    ready

--- src/main/java/servlet/Compete.java
+++ src/main/java/servlet/Compete.java
@@ -470,16 +470,14 @@ public class Compete extends HttpServlet
                     +   "</ul> \n"
                     +   "</li> \n"
                     +   "</ul> \n"
-
-                     +	"<button formaction='/LogOut' type='submit' class='btn btn-primary>Log Out</button> \n" 
                     +   "<form method='POST' action='/Compete' class='navbar-form navbar-right'> \n"
                     +   "<!-- Button trigger modal --> \n"
-                   
+                    +	"<button formaction = '/LogOut' type='submit' class='btn btn-primary'> Log Out </button>"
                     +   "<button type='submit' class='btn btn-primary'> \n"
                     +   " Update Scoreboard \n"
                     +   "</button> \n"
                     +   "</form> \n"
-                        //  +    "</div><!--/.nav-collapse --> \n";
+                        //  +    "</div><!--/.nav-collapse --> \n"
                     +   "</div><!--/.container-fluid --> \n"
                     +   "</nav> \n"
                     +   "<!-- End static navbar --> \n"
